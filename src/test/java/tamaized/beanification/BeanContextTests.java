7bd3f47718f1f0fb2f47f7d6a715a0d3f2ce4c68
Tamaized <9671313+Tamaized@users.noreply.github.com>
2024-10-13 23:40:35 -0400

    Add some general BeanContext tests

--- src/test/java/tamaized/beanification/BeanContextTests.java
+++ src/test/java/tamaized/beanification/BeanContextTests.java
@@ -1,13 +1,42 @@
 package tamaized.beanification;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tamaized.beanification.junit.TestConstants;
 
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+
 public class BeanContextTests {
+
+	@BeforeEach
+	public void beforeEach() {
+		BeanContext.INSTANCE = spy(BeanContext.class);
+	}
 
 	@Test
 	public void contextLoads() {
-		BeanContext.init(TestConstants.MODID);
+		assertDoesNotThrow(() -> BeanContext.init(TestConstants.MODID));
+	}
+
+	@Test
+	public void unknownModContainer() {
+		assertThrows(NoSuchElementException.class, () -> BeanContext.init("missingno"));
+	}
+
+	@Test
+	public void directBeanRegistration() {
+		TestBean bean = new TestBean();
+		TestBean namedBean = new TestBean();
+		BeanContext.init(TestConstants.MODID, beanContextRegistrar -> {
+			beanContextRegistrar.register(TestBean.class, bean);
+			beanContextRegistrar.register(TestBean.class, "named", namedBean);
+		});
+
+		assertSame(bean, BeanContext.inject(TestBean.class));
+		assertSame(namedBean, BeanContext.inject(TestBean.class, "named"));
 	}
 
 	private static class TestBean {
