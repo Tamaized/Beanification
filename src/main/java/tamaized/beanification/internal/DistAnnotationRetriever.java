4fd73b3193bf5988239d49c8b2f560d74aa88119
Tamaized <9671313+Tamaized@users.noreply.github.com>
2024-10-18 11:38:58 -0400

    Fix possible heap pollution

--- src/main/java/tamaized/beanification/internal/DistAnnotationRetriever.java
+++ src/main/java/tamaized/beanification/internal/DistAnnotationRetriever.java
@@ -9,15 +9,20 @@ import org.jetbrains.annotations.ApiStatus;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 @ApiStatus.Internal
 public class DistAnnotationRetriever {
 
 	@SafeVarargs
+	@SuppressWarnings({"UseBulkOperation", "ManualArrayToCollectionCopy"})
 	public final Stream<ModFileScanData.AnnotationData> retrieve(ModFileScanData scanData, ElementType elementType, Class<? extends Annotation>... types) {
-		return Arrays.stream(types).flatMap(type -> scanData.getAnnotatedBy(type, elementType).filter(annotation -> {
+		List<Class<? extends Annotation>> t = new ArrayList<>();
+		for (Class<? extends Annotation> type : types) {
+			t.add(type);
+		}
+		return t.stream().flatMap(type -> scanData.getAnnotatedBy(type, elementType).filter(annotation -> {
 			if (annotation.annotationData().get("dist") instanceof ArrayList<?> list) {
 				if (list.isEmpty())
 					return true;
