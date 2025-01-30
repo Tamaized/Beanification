4401e538395b5c799422a50145138528c5aad710
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-01-30 10:06:05 -0500

    Add some logging

--- src/gradle/java/tamaized/beanification/gradle/asm/ConfigurableTransformer.java
+++ src/gradle/java/tamaized/beanification/gradle/asm/ConfigurableTransformer.java
@@ -5,7 +5,8 @@ import groovyjarjarasm.asm.Opcodes;
 
 public class ConfigurableTransformer {
 
-	public static void transform(MethodVisitor method) {
+	public static void transform(MethodVisitor method, String className) {
+		System.out.println("[Configurable] Transforming " + className);
 		method.visitVarInsn(Opcodes.ALOAD, 0);
 		method.visitMethodInsn(Opcodes.INVOKESTATIC, "tamaized/beanification/BeanContext", "injectInto", "(Ljava/lang/Object;)V", false);
 	}
