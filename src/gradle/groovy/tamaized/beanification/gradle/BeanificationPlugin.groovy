949ba2cc99bfceb76e13016ae83236b589a8f1cf
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-01-30 12:55:45 -0500

    This should work

--- src/gradle/groovy/tamaized/beanification/gradle/BeanificationPlugin.groovy
+++ src/gradle/groovy/tamaized/beanification/gradle/BeanificationPlugin.groovy
@@ -2,28 +2,17 @@ package tamaized.beanification.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy
 import tamaized.beanification.gradle.asm.CompileTimeTransformer
 
 class BeanificationPlugin implements Plugin<Project> {
 
-	private static final String LOCATION = "beanification-instrumented-classes"
-
 	@Override
 	void apply(Project project) {
-		def instrumentedDir = project.layout.buildDirectory.dir(LOCATION)
-
-		def copyTask = project.tasks.register("beanificationCopyClasses", Copy) {
-			it.dependsOn "classes"
-			it.from project.layout.buildDirectory.dir("classes/java/main")
-			it.into instrumentedDir
-		}
-
 		project.tasks.register("beanificationTransformClasses") {
-			it.dependsOn copyTask
+			it.dependsOn 'classes'
 
 			it.doLast {
-				instrumentedDir.get().asFileTree.matching {
+				project.layout.buildDirectory.dir("classes/java/main").get().asFileTree.matching {
 					it.include '**/*.class'
 				}.each { file ->
 					CompileTimeTransformer.processClassFile(file)
