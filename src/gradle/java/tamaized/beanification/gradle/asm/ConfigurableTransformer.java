faf9e7b8c51983324226be9092584b0df3d1b014
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-01-30 11:28:35 -0500

    Use a proper logger

--- src/gradle/java/tamaized/beanification/gradle/asm/ConfigurableTransformer.java
+++ src/gradle/java/tamaized/beanification/gradle/asm/ConfigurableTransformer.java
@@ -2,11 +2,15 @@ package tamaized.beanification.gradle.asm;
 
 import groovyjarjarasm.asm.MethodVisitor;
 import groovyjarjarasm.asm.Opcodes;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 
 public class ConfigurableTransformer {
 
+	private static final Logger logger = Logging.getLogger(ConfigurableTransformer.class);
+
 	public static void transform(MethodVisitor method, String className) {
-		System.out.println("[Configurable] Transforming " + className);
+		logger.info("Transforming {}", className);
 		method.visitVarInsn(Opcodes.ALOAD, 0);
 		method.visitMethodInsn(Opcodes.INVOKESTATIC, "tamaized/beanification/BeanContext", "injectInto", "(Ljava/lang/Object;)V", false);
 	}
