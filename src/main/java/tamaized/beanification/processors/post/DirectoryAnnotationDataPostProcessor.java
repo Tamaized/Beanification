112c6c5f6d65e21998ab03fd5bed1ea7e61ed1aa
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-02-06 11:16:21 -0500

    Use Class.forName

--- src/main/java/tamaized/beanification/processors/post/DirectoryAnnotationDataPostProcessor.java
+++ src/main/java/tamaized/beanification/processors/post/DirectoryAnnotationDataPostProcessor.java
@@ -59,7 +59,13 @@ public class DirectoryAnnotationDataPostProcessor implements AnnotationDataPostP
 
 	private List<?> injectList(BeanContext.BeanContextInternalInjector context, ModFileScanData scanData, Class<?> parent, Class<?> classFilter) {
 		return scanData.getClasses().stream()
-			.map(data -> data.clazz().getClass())
+			.map(data -> {
+				try {
+					return Class.forName(data.clazz().getClassName());
+				} catch (ClassNotFoundException e) {
+					throw new RuntimeException(e);
+				}
+			})
 			.filter(data -> data.getPackageName().equals(parent.getPackageName()))
 			.filter(classFilter::isAssignableFrom)
 			.filter(data -> context.contains(data, null))
